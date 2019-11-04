c05a8ae66ae4a07ebda67a5cf004c1f725e3f9f9
MathiasHolst <57090767+MathiasHolst@users.noreply.github.com>
2019-11-04 10:11:01 +0100

    Objekter med beskrivelse og funktioner

---
+++ OOP Classes/OOP Classes/Chair.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Classes
+{
+    class Chair
+    {
+        public int Legs;
+        public string color;
+        public string material;
+
+        public void description()
+        {
+            Console.WriteLine("This chair got " + Legs + " and it's made of " + material + ". The color of the chair is " + color);
+        }
+
+        public void moveChair()
+        {
+            Random rndnum = new Random();
+            int flytStol = rndnum.Next(1, 6);
+            switch(flytStol)
+            {
+                case 1: Console.WriteLine(" Chair");
+                    break;
+                case 2: Console.WriteLine("  Chair");
+                    break;
+                case 3: Console.WriteLine("   Chair");
+                    break;
+                case 4: Console.WriteLine("    Chair");
+                    break;
+                case 5: Console.WriteLine("     Chair");
+                    break;
+            }
+        }
+
+        public void flipChair()
+        {
+            Console.WriteLine("riahC");
+        }
+
+        public void ThrowChair()
+        {
+            Console.WriteLine("Chair");
+            Console.WriteLine("Throwing....");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("                                       Chair");
+        }
+    }
+}
---
+++ OOP Classes/OOP Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OOP_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chair minStol = new Chair();
+            minStol.color = "Black";
+            minStol.material = "Plastic";
+            minStol.Legs = 2;
+
+            Console.WriteLine("Chair description:");
+            minStol.description();
+            Console.WriteLine("Move Chair:");
+            minStol.moveChair();
+            Console.WriteLine("Flip chair:");
+            minStol.flipChair();
+            Console.WriteLine("Throw the chair:");
+            minStol.ThrowChair();
+            Console.WriteLine("--------------------------------------------------");
+
+            Lamp minLampe = new Lamp();
+            minLampe.color = "white";
+            minLampe.shape = "Squared";
+            minLampe.material = "plastic";
+            minLampe.height = 15;
+            minLampe.width = 15;
+
+            Console.WriteLine("Lamp description:");
+            minLampe.description();
+            Console.WriteLine("Lamp turning off:");
+            minLampe.TurnOff();
+            Console.WriteLine("Lamp turning on:");
+            minLampe.TurnOn();
+        }
+    }
+}
