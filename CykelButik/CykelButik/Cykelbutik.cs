6251b67590a22c878350673a924e7b7a3afca808
Mathias <holsthategaming@gmail.com>
2019-11-07 14:00:38 +0100

    Issue fix #4

--- CykelButik/CykelButik/Cykelbutik.cs
+++ CykelButik/CykelButik/Cykelbutik.cs
@@ -6,40 +6,38 @@ namespace CykelButik
 {
     class Cykelbutik
     {
-        Cykler cykel1 = new Cykler("MerckX", 17.5, "Hvid");
-        Cykler cykel2 = new Cykler("MerckXRacer", 20.0, "Sort");
-
+        public List<Cykler> ListeAfCykler = new List<Cykler>();
         public string hjulstørrelserOverDetGivne(double index)
         {
-            string[] CykelNavne = { cykel1.mærke, cykel2.mærke };
-            if(index > cykel1.hjulstørrelse || index > cykel2.hjulstørrelse)
+            string[] CykelNavne = { ListeAfCykler[0].mærke, ListeAfCykler[1].mærke };
+            if(index > ListeAfCykler[0].hjulstørrelse || index > ListeAfCykler[1].hjulstørrelse)
             {
-                Console.WriteLine("Der er ikke nogen hjulstørrelse over det indskrevne.");
+                return "Der er ikke nogen hjulstørrelse over det indskrevne.";
             }
-            else if (cykel1.hjulstørrelse > index && cykel2.hjulstørrelse < index)
+            else if (ListeAfCykler[0].hjulstørrelse > index && ListeAfCykler[1].hjulstørrelse < index)
             {
-                return cykel1.mærke;
+                return ListeAfCykler[0].mærke;
             }
-            else if (cykel1.hjulstørrelse < index && cykel2.hjulstørrelse > index)
+            else if (ListeAfCykler[0].hjulstørrelse < index && ListeAfCykler[1].hjulstørrelse > index)
             {
-                return cykel2.mærke;
+                return ListeAfCykler[1].mærke;
             }
             return CykelNavne.ToString();
         }
         public string hjulstørrelserUnderDetGivne(double index)
         {
-            string[] CykelNavne = { cykel1.mærke, cykel2.mærke };
-            if(index < cykel1.hjulstørrelse && index < cykel2.hjulstørrelse)
+            string[] CykelNavne = { ListeAfCykler[0].mærke, ListeAfCykler[1].mærke };
+            if(index < ListeAfCykler[0].hjulstørrelse && index < ListeAfCykler[1].hjulstørrelse)
             {
-                Console.WriteLine("Der er ikke nogen hjulstørrelse under det indskrevne.");
+                return "Der er ikke nogen hjulstørrelse under det indskrevne.";
             }
-            else if (cykel1.hjulstørrelse < index && cykel2.hjulstørrelse > index)
+            else if (ListeAfCykler[0].hjulstørrelse < index && ListeAfCykler[1].hjulstørrelse > index)
             {
-                return cykel1.mærke;
+                return ListeAfCykler[0].mærke;
             }
-            else if (cykel1.hjulstørrelse > index && cykel2.hjulstørrelse < index)
+            else if (ListeAfCykler[0].hjulstørrelse > index && ListeAfCykler[1].hjulstørrelse < index)
             {
-                return cykel2.mærke;
+                return ListeAfCykler[1].mærke;
             }
             return CykelNavne.ToString();
         }
--- CykelButik/CykelButik/Program.cs
+++ CykelButik/CykelButik/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CykelButik
 {
@@ -6,7 +7,21 @@ namespace CykelButik
     {
         static void Main(string[] args)
         {
-            
+            Cykler cykel1 = new Cykler("Fast track", 32.0, "Sort");
+            Cykler cykel2 = new Cykler("Dex", 28.0, "Gul");
+            Cykelbutik MinButik = new Cykelbutik();
+
+            MinButik.ListeAfCykler.Add(cykel1);
+            MinButik.ListeAfCykler.Add(cykel2);
+
+            int i = 0;
+            while (i < MinButik.ListeAfCykler.Count)
+            {
+                Console.WriteLine(MinButik.ListeAfCykler[i].mærke);
+                Console.WriteLine(MinButik.ListeAfCykler[i].hjulstørrelse);
+                Console.WriteLine(MinButik.ListeAfCykler[i].farve);
+                i++;
+            }
         }
     }
 }
