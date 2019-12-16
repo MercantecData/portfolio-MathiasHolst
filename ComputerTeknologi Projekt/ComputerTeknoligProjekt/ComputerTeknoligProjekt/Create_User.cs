9235943d6a36cc3b57f5d961eca3ae751a8401e2
Mathias <holsthategaming@gmail.com>
2019-12-16 14:43:55 +0100

    Første User lavet + gået igang med nogle funktioner for interfaces

---
+++ ComputerTeknologi Projekt/ComputerTeknoligProjekt/ComputerTeknoligProjekt/Create_User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerTeknoligProjekt
+{
+    class Create_User
+    {
+        public string first_Name;
+        public string last_Name;
+        public int age;
+        public string address;
+        public int zipcode;
+
+        public Create_User(string first_Name, string last_Name, int age, string address, int zipcode)
+        {
+            this.first_Name = first_Name;
+            this.last_Name = last_Name;
+            this.age = age;
+            this.address = address;
+            this.zipcode = zipcode;
+        }
+    }
+}
---
+++ ComputerTeknologi Projekt/ComputerTeknoligProjekt/ComputerTeknoligProjekt/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ComputerTeknoligProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Fornavn: ");
+            string first_Name = Console.ReadLine();
+            Console.Write("Efternavn: ");
+            string last_Name = Console.ReadLine();
+            Console.Write("Alder: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Addresse: ");
+            string address = Console.ReadLine();
+            Console.Write("Postnummer: ");
+            int zipcode = int.Parse(Console.ReadLine());
+
+            if(age > 100)
+            {
+                Console.WriteLine("You are too old to create an account on this application");
+                return;
+            }
+            Console.Clear();
+            Console.WriteLine("Bruger oprettet");
+            Create_User user1 = new Create_User(first_Name, last_Name, age, address, zipcode);
+
+            Console.WriteLine($"{user1.first_Name} {last_Name} {age} år gammel");
+            Console.Clear();
+
+            if (user1.age >= 18)
+            {
+                above18();
+            } else if (user1.age < 18)
+            {
+                below18();
+            }
+
+            Console.WriteLine("asdsadsadsadasasdsads");
+            Console.WriteLine("sadsadsadsadsadsadsad");
+        }
+
+        static void above18()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+        }
+
+        static void below18()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+        }
+    }
+}
