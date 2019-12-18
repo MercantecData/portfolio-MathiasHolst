4d6e7b8a119e96be7ac434f6825e7983233531ba
Mathias <holsthategaming@gmail.com>
2019-12-18 12:27:13 +0100

    Relations + Users added
    Der er nu added 3 users og man kan se om de har mulige relationer til hinanden

--- ComputerTeknologi Projekt/ComputerTeknoligProjekt/ComputerTeknoligProjekt/Create_User.cs
+++ ComputerTeknologi Projekt/ComputerTeknoligProjekt/ComputerTeknoligProjekt/Create_User.cs
@@ -43,5 +43,78 @@ namespace ComputerTeknoligProjekt
         {
             Console.ForegroundColor = ConsoleColor.Blue;
         }
+
+        public void d()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("Everything that showed up true is a number in your name which you now need to change");
+
+            //Tjekker om din alder er over 100 og hvis den er det så skal du ændre den
+            while (age > 100)
+            {
+                Console.WriteLine("You are too old to create an account on this application pls write your real age.");
+                Console.Write("Age: ");
+                age = int.Parse(Console.ReadLine());
+            }
+            Create_User user1 = new Create_User(first_Name, last_Name, age, address, zipcode);
+
+            //Prøver at gøre sådan man ikke kan skrive tal i sit navn
+            Console.WriteLine("Press enter to check firstname for integers");
+            Console.ReadLine();
+            user1.CheckForNumbersInName(first_Name);
+
+            Console.WriteLine("Press enter to check lastname for integers");
+            Console.ReadLine();
+            user1.CheckForNumbersInName(user1.last_Name);
+
+
+            Console.WriteLine($"{user1.first_Name} {user1.last_Name} {age} yr");
+
+            //De 2 forskellige interfaces for en person over eller under 18
+            if (user1.age >= 18)
+            {
+                user1.above18();
+                int adultID = 1000;
+                //Du kan kun skrive 3 cifre i dit id
+                while (adultID > 999)
+                {
+                    Console.Write("If my calculations are right you should be at a age 18 or above \nso pls right your first 3-digits in your id: ");
+                    adultID = int.Parse(Console.ReadLine());
+                    if (adultID > 999)
+                    {
+                        Console.WriteLine("You should only write the first 3-digits in your id no more than that (Press enter to try again)");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    Console.WriteLine(adultID);
+                }
+                above18 user1above18 = new above18(adultID);
+                Console.Write("Write quit if you created all users you want to: ");
+            }
+            else if (user1.age < 18)
+            {
+                user1.below18();
+                int parentID = 1000;
+                //Du kan kun skrive 3 cifre i dit id
+                while (parentID > 999)
+                {
+                    Console.Write($"If my calculations are right you should be at a age below 18 \nso pls right your moms/dads first 3-digits in their id: ");
+                    parentID = int.Parse(Console.ReadLine());
+                    below18 user1below18 = new below18(parentID);
+                    if (parentID > 999)
+                    {
+                        Console.WriteLine("You should only write the first 3-digits in your id no more than that (Press enter to try again)");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    Console.WriteLine(parentID);
+                }
+            }
+            Console.WriteLine("User created");
+        }
     }
 }
--- ComputerTeknologi Projekt/ComputerTeknoligProjekt/ComputerTeknoligProjekt/Program.cs
+++ ComputerTeknologi Projekt/ComputerTeknoligProjekt/ComputerTeknoligProjekt/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ComputerTeknoligProjekt
 {
@@ -7,7 +8,9 @@ namespace ComputerTeknoligProjekt
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<Create_User> newList = new List<Create_User>();
 
+            //Oprettelse af user1
             Console.Write("Firstname: ");
             string first_Name = Console.ReadLine();
             Console.Write("Lastname: ");
@@ -19,65 +22,93 @@ namespace ComputerTeknoligProjekt
             Console.Write("Postalcode: ");
             int zipcode = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Everything that showed up true is a number in your name which you now need to change");
-
-            //Tjekker om din alder er over 100 og hvis den er det så skal du ændre den
-            while (age > 100)
-            {
-                Console.WriteLine("You are too old to create an account on this application pls write your real age.");
-                Console.Write("Age: ");
-                age = int.Parse(Console.ReadLine());
-            }
-            Console.WriteLine("User created");
             Create_User user1 = new Create_User(first_Name, last_Name, age, address, zipcode);
 
-            //Prøver at gøre sådan man ikke kan skrive tal i sit navn
-            Console.WriteLine("Press enter to check firstname for integers");
-            Console.ReadLine();
-            user1.CheckForNumbersInName(first_Name);
+            user1.Create();
+            user1.d();
+            newList.Add(user1);
+            Console.WriteLine($"Skriv {"stop"} for ikke at oprette flere brugere");
+            string stop = Console.ReadLine().Trim().ToLower();
 
-            Console.WriteLine("Press enter to check lastname for integers");
-            Console.ReadLine();
-            user1.CheckForNumbersInName(user1.last_Name);
-            
-
-            Console.WriteLine($"{user1.first_Name} {user1.last_Name} {age} yr");
-
-            //De 2 forskellige interfaces for en person over eller under 18
-            if (user1.age >= 18)
+            if (stop != "stop")
             {
-                user1.above18();
-                int adultID = 1000;
-                while (adultID > 999) {
-                    Console.Write("If my calculations are right you should be at a age 18 or above \nso pls right your first 3-digits in your id: ");
-                    adultID = int.Parse(Console.ReadLine());
-                    if (adultID > 999)
-                    {
-                        Console.WriteLine("You should only write the first 3-digits in your id no more than that (Press enter to try again)");
-                        Console.ReadLine();
-                        Console.Clear();
-                    }
-                    Console.WriteLine(adultID);
-                }
-                above18 user1above18 = new above18(adultID);
-            } else if (user1.age < 18)
-            {
-                user1.below18();
-                int parentID = 1000;
-                while (parentID > 999)
+                //Oprettelse af user2
+                Console.Write("Firstname: ");
+                first_Name = Console.ReadLine();
+                Console.Write("Lastname: ");
+                last_Name = Console.ReadLine();
+                Console.Write("Age: ");
+                age = int.Parse(Console.ReadLine());
+                Console.Write("Address: ");
+                address = Console.ReadLine();
+                Console.Write("Postalcode: ");
+                zipcode = int.Parse(Console.ReadLine());
+
+
+                Create_User user2 = new Create_User(first_Name, last_Name, age, address, zipcode);
+
+                user2.Create();
+                user2.d();
+                newList.Add(user2);
+
+                Console.WriteLine($"Skriv {"stop"} for ikke at oprette flere brugere");
+                stop = Console.ReadLine().Trim().ToLower();
+
+                if (stop != "stop")
                 {
-                    Console.Write($"If my calculations are right you should be at a age below 18 \nso pls right your moms/dads first 3-digits in their id: ");
-                    parentID = int.Parse(Console.ReadLine());
-                    below18 user1below18 = new below18(parentID);
-                    if (parentID > 999)
-                    {
-                        Console.WriteLine("You should only write the first 3-digits in your id no more than that (Press enter to try again)");
-                        Console.ReadLine();
-                        Console.Clear();
-                    }
-                    Console.WriteLine(parentID);
+                    //Oprettelse af user3
+                    Console.Write("Firstname: ");
+                    first_Name = Console.ReadLine();
+                    Console.Write("Lastname: ");
+                    last_Name = Console.ReadLine();
+                    Console.Write("Age: ");
+                    age = int.Parse(Console.ReadLine());
+                    Console.Write("Address: ");
+                    address = Console.ReadLine();
+                    Console.Write("Postalcode: ");
+                    zipcode = int.Parse(Console.ReadLine());
+
+                    Create_User user3 = new Create_User(first_Name, last_Name, age, address, zipcode);
+
+                    user3.Create();
+                    user3.d();
+                    Console.Clear();
+                    newList.Add(user3);
                 }
             }
+
+            Console.WriteLine("User's in the system");
+            Console.WriteLine(newList[0].first_Name);
+            try
+            {
+                Console.WriteLine(newList[1].first_Name);
+            }
+            catch (Exception) { }
+            try
+            {
+                Console.WriteLine(newList[2].first_Name);
+            }
+            catch (Exception) { }
+            Console.ReadLine();
+
+            try
+            {
+                if (newList[0].address == newList[1].address || newList[0].last_Name == newList[1].last_Name)
+                {
+                    Console.WriteLine($"{newList[0].first_Name} may have relations with {newList[1].first_Name}");
+                }
+            } catch (Exception) { }
+            try
+            {
+                if (newList[0].address == newList[2].address || newList[0].last_Name == newList[2].last_Name)
+                {
+                    Console.WriteLine($"{newList[0].first_Name} may have relations with {newList[2].first_Name}");
+                }
+                if (newList[1].address == newList[2].address || newList[1].last_Name == newList[2].last_Name)
+                {
+                    Console.WriteLine($"{newList[1].first_Name} may have relations with {newList[2].first_Name}");
+                }
+            } catch (Exception) { }
         }
 
     }
